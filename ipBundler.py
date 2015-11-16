946185e0e6b7e4636ed0a24554dbab1b5355ac02
Raghav <raghuram002@BrindaVidhur.domain.name>
2015-11-16 08:15:35 +0530

    init spec 1.0

--- ipBundler.py
+++ ipBundler.py
@@ -1,16 +1,47 @@
 #!/usr/bin/env python3.4
 
-import sys
+import os,sys
 from PyQt4 import QtGui, QtCore
+import subprocess
+from PyQt4.QtGui import *
+from PyQt4.QtCore import *
 
+
+from search import SearchThread
 
 
 class MyWindow(QtGui.QWidget):
     def __init__(self, parent=None):
         super(MyWindow, self).__init__(parent)
         self.model = QtGui.QFileSystemModel(self)
-        path = "/home/raghuram002/development/src/code"
-        self.model.setRootPath(path)
+        self.status = QtGui.QStatusBar(self)
+        self.status.showMessage("Ready")
+        self.labelFileName = QtGui.QLabel(self)
+        self.labelFileName.setText("Directory Name:")
+        self.query = QtGui.QLineEdit(self)
+        
+        """
+            Default is to start in user current directory
+        """
+        
+        self.query.setText(os.environ['PWD'])
+
+        self.labelFilePath = QtGui.QLabel(self)
+        self.labelFilePath.setText("Search Pattern:")
+        self.lineEditFilePath = QtGui.QLineEdit(self)
+        
+        self.buttonFileSearch = QtGui.QPushButton(self)
+        self.buttonFileSearch.setText("Search")
+        self.buttonFileSearch.clicked.connect(self.searchButton)
+        self.query.returnPressed.connect(self.buttonFileSearch.click)
+        self.query.setFocus()
+        path = self.query.text()
+        self.initpath = path
+        self.initTree()
+
+    def initTree(self):
+        print (self.initpath)
+        self.model.setRootPath(self.initpath)
         self.model.setReadOnly(False)
         self.indexRoot = self.model.index(self.model.rootPath())
         self.treeView = QtGui.QTreeView(self)
@@ -24,25 +55,63 @@ class MyWindow(QtGui.QWidget):
         self.treeView.setSelectionMode(QtGui.QAbstractItemView.ExtendedSelection)
         self.treeView.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
         self.treeView.customContextMenuRequested.connect(self.openMenu)
+        self.layout()
         
-        self.labelFileName = QtGui.QLabel(self)
-        self.labelFileName.setText("File Name:")
-
-        self.lineEditFileName = QtGui.QLineEdit(self)
-
-        self.labelFilePath = QtGui.QLabel(self)
-        self.labelFilePath.setText("File Path:")
-
-        self.lineEditFilePath = QtGui.QLineEdit(self)
-
+    def layout(self):
         self.gridLayout = QtGui.QGridLayout()
         self.gridLayout.addWidget(self.labelFileName, 0, 0)
-        self.gridLayout.addWidget(self.lineEditFileName, 0, 1)
+        self.gridLayout.addWidget(self.query, 0, 1)
         self.gridLayout.addWidget(self.labelFilePath, 1, 0)
         self.gridLayout.addWidget(self.lineEditFilePath, 1, 1)
+        self.gridLayout.addWidget(self.buttonFileSearch, 1, 2)
         self.layout = QtGui.QVBoxLayout(self)
         self.layout.addLayout(self.gridLayout)
         self.layout.addWidget(self.treeView)
+        self.layout.addWidget(self.status)
+        QShortcut(QKeySequence("Ctrl+Q"), self, self.close)
+
+
+    def relayout(self):
+        layout = self.layout
+        if layout != None:
+            self.treeViewWidget = self.layout.addWidget(self.treeView)
+            self.layout.addWidget(self.status)
+
+
+    def refreshTree(self):
+        Newpath = self.query.text()
+        if Newpath != self.initpath:
+            print (Newpath)
+            self.status.showMessage("Searching...")
+
+            self.treeView.deleteLater()
+            self.treeView.setParent(None)
+            self.status.setParent(None)
+            self.initpath = Newpath
+            self.model.setRootPath(self.initpath)
+            self.model.setReadOnly(False)
+            self.indexRoot = self.model.index(self.model.rootPath())
+            self.treeView = QtGui.QTreeView(self)
+            self.treeView.setModel(self.model)
+            self.treeView.setRootIndex(self.indexRoot)
+            self.treeView.clicked.connect(self.on_treeView_clicked)
+            self.treeView.setDragEnabled(True)
+            self.treeView.setAcceptDrops(True)
+            self.treeView.setDropIndicatorShown(True)
+            self.treeView.setDragDropMode(QtGui.QAbstractItemView.InternalMove)
+            self.treeView.setSelectionMode(QtGui.QAbstractItemView.ExtendedSelection)
+            self.treeView.setContextMenuPolicy(QtCore.Qt.CustomContextMenu)
+            self.treeView.customContextMenuRequested.connect(self.openMenu)
+            self.relayout()
+            self.status.showMessage("Ready")
+
+        else:
+            self.status.showMessage("Ready.")
+
+
+
+
+
 
     
 
@@ -58,18 +127,29 @@ class MyWindow(QtGui.QWidget):
                 level += 1
         
         menu = QtGui.QMenu()
-        menu.addAction(self.tr("Edit object"))
-        menu.addAction(self.tr("Delete object"))
+        menu.addAction("Open", self.open_treeObj)
+        menu.addAction("Delete", self.delete_treeObj)
         menu.exec_(self.treeView.viewport().mapToGlobal(position))
+    
+    def open_treeObj(self):
+        print ("Open Object")
+        self.ff.show()
+    
+    def delete_treeObj(self):
+        print ("Delete Object")
+        root = self.treeView.invisibleRootItem()
+        for item in self.selectedItems():
+            (item.parent() or root).removeChild(item)
     
     def on_treeView_clicked(self, index):
         indexItem = self.model.index(index.row(), 0, index.parent())
-
         fileName = self.model.fileName(indexItem)
         filePath = self.model.filePath(indexItem)
 
-        self.lineEditFileName.setText(fileName)
-        self.lineEditFilePath.setText(filePath)
+
+    def searchButton(self):
+        print ("Starting Search")
+        self.refreshTree()
 
 
 def main():
